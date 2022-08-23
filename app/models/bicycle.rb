0ca60dfdc0beeca686fa14368b274b1199885463
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-08-23 16:19:19 +0200

    create model review

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -1,11 +1,10 @@
 class Bicycle < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews
   validates :title, :category, :price_per_day, :size, :rating, presence: true
   # validates :photo, presence: true
-  validates :rating, numericality: { only_integer: true }
-  validates :rating, numericality: { in: 0..5}
-  validates :size, numericality: { in: 10..70}
+  validates :size, numericality: { in: 10..70 }
   validates :title, length: { maximum: 30 }
   validates :category, inclusion: { in: ['enfant', 'VTC', 'ville', 'cargo', 'tandem'] }
 end
--- db/migrate/20220823133404_add_rating_to_bicycles.rb
+++
@@ -1,5 +0,0 @@
-class AddRatingToBicycles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :bicycles, :rating, :integer
-  end
-end
