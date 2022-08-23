cc4ebcd5861698f861fdab2a17e5fc2364d3005f
Samuel Paget <sjapaget@gmail.com>
2022-08-23 12:12:00 +0200

    Add total_price method to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :bicycle
   belongs_to :user
   validates :start_date, :end_date, presence: true
+
+  def total_price
+    # Calculate the duration of the rental
+    duration = (self.end_date - self.start_date).to_i
+
+    # Multiply duration of rental by the bicycle's price per day
+    duration * self.bicycle.price_per_day
+  end
 end
