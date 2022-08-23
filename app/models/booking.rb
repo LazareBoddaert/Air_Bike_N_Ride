1cbd113837d31b13be7f8b905686d0482541fd55
Samuel Paget <sjapaget@gmail.com>
2022-08-23 13:15:22 +0200

    Add validation to prevent bookings being created for past dates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :bicycle
   belongs_to :user
   validates :start_date, :end_date, presence: true
+  # end_date must be after start_date
   validates :end_date, comparison: { greater_than: :start_date }
+  # start_date must be after today's date
+  validates :start_date, comparison: { greater_than: Date.today }
 
   def total_price
     # Calculate the duration of the rental
