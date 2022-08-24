c919dab2c7db369b5404c0d794472c81d81e104f
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-08-24 12:09:54 +0200

    views show bicycle

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -2,7 +2,7 @@ class Bicycle < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
-  validates :title, :category, :price_per_day, :size, :rating, presence: true
+  validates :title, :category, :price_per_day, :size, presence: true
   # validates :photo, presence: true
   validates :size, numericality: { in: 10..70 }
   validates :title, length: { maximum: 30 }
