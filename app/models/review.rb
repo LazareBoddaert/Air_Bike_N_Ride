d920ec85d94c66cd8749e87415b23dd5c2852940
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-08-25 17:33:48 +0200

    finish back review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :booking
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: { in: 0..5 }
-  validates :content, presence: true
+  validates :content, :rating, presence: true
 
 
 end
