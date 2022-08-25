c72616e20c38e00225ca8e240a1968413a890935
Samuel Paget <sjapaget@gmail.com>
2022-08-25 15:26:05 +0200

    Add card carousel to dash

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+Booking.destroy_all
 Bicycle.destroy_all
 User.destroy_all
 user1 = User.create!(username: "user1", email: "test@test.fr", password: '123456')
