5448bd4594162307d0e602400bffc1a40a54c89b
Youssef Aissaoui <aissa.youssef45@gmail.com>
2022-08-25 08:54:57 +0200

    modified model review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :bicycle
-  validates :content, :rating, presence: true
+  belongs_to :booking
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: { in: 0..5 }
+  validates :content, presence: true
 end
