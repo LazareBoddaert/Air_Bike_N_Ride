43a7166c767a35851a14d3ed96bd3ec23d6fb80c
Lazare Boddaert <lazare.boddaert@gmail.com>
2022-08-25 16:59:09 +0200

    User can rent their bike

--- app/controllers/bicycles_controller.rb
+++ app/controllers/bicycles_controller.rb
@@ -1,6 +1,6 @@
 class BicyclesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_bicycle, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_bicycle, only: %i[show edit update destroy]
 
   def index
     @bicycles = policy_scope(Bicycle)
